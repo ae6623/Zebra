0594cf8c6fc43141ac0ee24242cfc0acef5bc583
ae6623 <394263788@qq.com>
2016-11-16 00:48:02 +0800

    zebra-boot Add

--- zebra4j/zebra-boot/src/main/java/com/zebra/boot/App.java
+++ zebra4j/zebra-boot/src/main/java/com/zebra/boot/App.java
@@ -1,10 +1,23 @@
 package com.zebra.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by lzy@js-dev.cn on 2016/11/15 0015.
  */
-//@RestController
-//@SpringBootApplication
+@RestController
+@SpringBootApplication
 public class App {
 
+	@RequestMapping("/")
+	String boot(){
+		return " Zebra is booting  ... ";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
