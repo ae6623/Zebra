2d6e6fe6902dc3e46601a41a2cd6d1c4844247f5
ae6623 <394263788@qq.com>
2016-11-29 23:49:49 +0800

    add zookeeper-config zk.js

---
+++ zebra4js/src/common/config/zk.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * config zk
+ */
+export default {
+    //key: value
+    HOST_PORT: 1234,
+    ZK_SERVER: '127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183',
+    REGISTRY_ROOT: '/registry'
+};
